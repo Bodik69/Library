565e30a1cc532f17b93c3cf3bc0d7de63afe0b5d
bashenskii <bashenskii@gmail.com>
2015-12-09 13:59:48 +0200

    add many to many

--- src/main/java/com/softserve/edu/entity/Author.java
+++ src/main/java/com/softserve/edu/entity/Author.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.Set;
 
 /**
  * Created by Ihor Sokolyk on 09.12.2015.
@@ -23,4 +24,19 @@ public class Author {
 
     @Column(name="lastName")
     private String lastName;
+
+    private Set<Book> books;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinTable(name = "coauthor", joinColumns = {
+            @JoinColumn(name = "idAuthor", nullable = false, updatable = false) },
+            inverseJoinColumns = { @JoinColumn(name = "code",
+                    nullable = false, updatable = false) })
+    public Set<Book> getBooks() {
+        return this.books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
 }
--- src/main/java/com/softserve/edu/entity/Book.java
+++ src/main/java/com/softserve/edu/entity/Book.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.Set;
 
 /**
  * Created by Богдан on 09.12.2015.
@@ -36,4 +37,15 @@ public class Book {
 
     @Column(name="copyCount")
     private Integer copyCount;
+
+    private Set<Author> authors;
+
+    @ManyToMany(fetch = FetchType.LAZY, mappedBy = "books")
+    public Set<Author> getAuthors() {
+        return this.authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
 }
