a00141e8575b154e1610d3dd13672cc0c4cc615f
persik <ihorsokolyk1993@gmail.com>
2015-12-09 14:38:44 +0200

    bla

--- src/main/java/com/softserve/edu/Main.java
+++ src/main/java/com/softserve/edu/Main.java
@@ -1,21 +1,26 @@
 package com.softserve.edu;
 
 import com.softserve.edu.entity.Author;
+import com.softserve.edu.entity.Reader;
 import com.softserve.edu.util.HibernateUtil;
 import org.hibernate.Session;
+
+import java.sql.Date;
 
 /**
  * Created by Ihor Sokolyk on 09.12.2015.
  */
 public class Main {
     public static void main(String[] args) {
-        Author author = new Author();
-        author.setFirstName("John");
-        author.setLastName("Doe");
+        Reader reader = new Reader();
+        reader.setAdress("sdsdc");
+        reader.setBirth("sdsdfsdf");
+        reader.setName("sfsfsdf");
+        reader.setPhone("sdfsdfsdfsdf");
         Session session = null;
         try { session = HibernateUtil.getSessionFactory().openSession();
             session.beginTransaction();
-            session.save(author);
+            session.save(reader);
             session.getTransaction().commit();
         } finally {
             if ((session != null) && (session.isOpen())) {  session.close();   }
