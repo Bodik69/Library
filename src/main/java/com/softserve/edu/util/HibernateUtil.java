63bddcbe0d56b394affc7a8233a854140b6be82d
Bodik69 <bsmachylo86@gmail.com>
2015-12-09 17:52:48 +0200

    change hibernate util

--- src/main/java/com/softserve/edu/util/HibernateUtil.java
+++ src/main/java/com/softserve/edu/util/HibernateUtil.java
@@ -2,9 +2,9 @@ package com.softserve.edu.util;
 
 import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
-import org.hibernate.service.ServiceRegistryBuilder;
 
 /**
  * Created by Ihor Sokolyk on 09.12.2015.
@@ -19,7 +19,7 @@ public class HibernateUtil {
         {
             Configuration configuration = new Configuration().configure();
 
-            serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
+            serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
             sessionFactory = configuration.buildSessionFactory(serviceRegistry);
         }
         catch (HibernateException he)
