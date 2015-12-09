a4f372f95911cf8fff93934e559e0d99cea0aa1a
persik <ihorsokolyk1993@gmail.com>
2015-12-09 18:08:32 +0200

    fixed birthDate in Reader

--- src/main/java/com/softserve/edu/Main.java
+++ src/main/java/com/softserve/edu/Main.java
@@ -1,10 +1,9 @@
 package com.softserve.edu;
-import com.softserve.edu.entity.Author;
+
 import com.softserve.edu.entity.Reader;
 import com.softserve.edu.util.HibernateUtil;
 import org.hibernate.Session;
 
-import java.sql.Date;
 
 /**
  * Created by Ihor Sokolyk on 09.12.2015.
@@ -13,7 +12,6 @@ public class Main {
     public static void main(String[] args) {
         Reader reader = new Reader();
         reader.setAdress("sdsdc");
-        reader.setBirth("sdsdfsdf");
         reader.setName("sfsfsdf");
         reader.setPhone("sdfsdfsdfsdf");
         Session session = null;
