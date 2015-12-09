b7d7fc7b896072c3efd9a9b83668b81b3a444015
Bodik69 <bsmachylo86@gmail.com>
2015-12-09 20:27:02 +0200

    edit main

--- src/main/java/com/softserve/edu/Main.java
+++ src/main/java/com/softserve/edu/Main.java
@@ -1,5 +1,7 @@
 package com.softserve.edu;
 
+import com.softserve.edu.dao.impl.DaoFactory;
+import com.softserve.edu.entity.Book;
 import com.softserve.edu.entity.Reader;
 import com.softserve.edu.util.HibernateUtil;
 import org.hibernate.Session;
@@ -11,14 +13,8 @@ import org.hibernate.Session;
 public class Main {
     public static void main(String[] args) {
 
-        Session session = null;
-        try { session = HibernateUtil.getSessionFactory().openSession();
-            session.beginTransaction();
-
-            session.getTransaction().commit();
-        } finally {
-            if ((session != null) && (session.isOpen())) {  session.close();   }
-        }
+        Book book = DaoFactory.getInstance().getBookDAO().find(1);
+        System.out.println(book.getAuthor().getFirstName());
     }
 }
 
