e501645ad258dae481a8e2a7ff1e9f0464b04405
persik <ihorsokolyk1993@gmail.com>
2015-12-09 14:21:28 +0200

    try v1.0

---
+++ src/main/java/com/softserve/edu/Main.java
@@ -0,0 +1,24 @@
+package com.softserve.edu;
+
+import com.softserve.edu.entity.Author;
+import com.softserve.edu.util.HibernateUtil;
+import org.hibernate.Session;
+
+/**
+ * Created by Ihor Sokolyk on 09.12.2015.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Author author = new Author();
+        author.setFirstName("John");
+        author.setLastName("Doe");
+        Session session = null;
+        try { session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.save(author);
+            session.getTransaction().commit();
+        } finally {
+            if ((session != null) && (session.isOpen())) {  session.close();   }
+        }   }
+}
+
