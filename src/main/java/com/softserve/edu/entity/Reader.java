7683c6edf19dce80cb00d8d589c622bb6025251a
bashenskii <bashenskii@gmail.com>
2015-12-09 13:31:11 +0200

    some settings

--- src/main/java/com/softserve/edu/entity/Reader.java
+++ src/main/java/com/softserve/edu/entity/Reader.java
@@ -1,10 +1,14 @@
 package com.softserve.edu.entity;
+import jdk.nashorn.internal.objects.annotations.Getter;
+import lombok.*;
+
 import javax.persistence.*;
 
 /**
  * Created by Ruslan on 09.12.2015.
  */
-
+@lombok.Getter
+@Setter
 @Entity
 @Table(name = "reader")  // @Table is optional
 public class Reader {
@@ -12,72 +16,24 @@ public class Reader {
     @Column(name = "idReader")
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer idReader;
+
     @Column(name = "name")
     private String name;
+
     @Column(name = "surname")
     private String surname;
+
     @Column(name = "adress")
     private String adress;
+
     @Column(name = "phone")
     private String phone;
+
     @Column(name = "birth")
     private String birth;
+
     @Column(name = "dateOfCreate")
     private String dateOfCreate;
 
-    public Integer getIdReader() {
-        return idReader;
-    }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public String getAdress() {
-        return adress;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public String getBirth() {
-        return birth;
-    }
-
-    public String getDateOfCreate() {
-        return dateOfCreate;
-    }
-
-    public void setIdReader(Integer idReader) {
-        this.idReader = idReader;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public void setAdress(String adress) {
-        this.adress = adress;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public void setBirth(String birth) {
-        this.birth = birth;
-    }
-
-    public void setDateOfCreate(String dateOfCreate) {
-        this.dateOfCreate = dateOfCreate;
-    }
 }
