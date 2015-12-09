4633605eb65ce98b9a716d4158b20d338fb94419
persik <ihorsokolyk1993@gmail.com>
2015-12-09 20:02:55 +0200

    delete from main

--- src/main/java/com/softserve/edu/Main.java
+++ src/main/java/com/softserve/edu/Main.java
@@ -10,19 +10,15 @@ import org.hibernate.Session;
  */
 public class Main {
     public static void main(String[] args) {
-//        Reader reader = new Reader();
-//        reader.setAdress("sdsdc");
-//        reader.setName("sfsfsdf");
-//
-//        reader.setPhone("sdfsdfsdfsdf");
-//        Session session = null;
-//        try { session = HibernateUtil.getSessionFactory().openSession();
-//            session.beginTransaction();
-//            session.save(reader);
-//            session.getTransaction().commit();
-//        } finally {
-//            if ((session != null) && (session.isOpen())) {  session.close();   }
-//        }
+
+        Session session = null;
+        try { session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+
+            session.getTransaction().commit();
+        } finally {
+            if ((session != null) && (session.isOpen())) {  session.close();   }
+        }
     }
 }
 
