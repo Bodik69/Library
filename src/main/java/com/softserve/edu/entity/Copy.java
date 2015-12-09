acefdb69a5ec482a69438ee988bbe8adb27362af
Bodik69 <bsmachylo86@gmail.com>
2015-12-09 14:02:12 +0200

    add copy entity

---
+++ src/main/java/com/softserve/edu/entity/Copy.java
@@ -0,0 +1,29 @@
+package com.softserve.edu.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+/**
+ * Created by Богдан on 09.12.2015.
+ */
+@Getter
+@Setter
+@Entity
+@Table(name = "copy")
+public class Copy {
+
+    @Id
+    @Column(name = "inventoryNumber")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "code")
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="code")
+    private Book book;
+
+    @Column(name = "isInStock")
+    private Boolean isInStock;
+}
