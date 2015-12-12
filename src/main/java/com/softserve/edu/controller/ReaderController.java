edfb6dc84ed11f361fe9e2284c7b04ee1fc588e4
persik <ihorsokolyk1993@gmail.com>
2015-12-12 17:40:30 +0200

    added insert and delete ability to reader

--- src/main/java/com/softserve/edu/controller/ReaderController.java
+++ src/main/java/com/softserve/edu/controller/ReaderController.java
@@ -1,10 +1,14 @@
 package com.softserve.edu.controller;
 
+import com.softserve.edu.entity.Reader;
 import com.softserve.edu.service.ReaderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.Map;
 
 /**
@@ -18,19 +22,28 @@ public class ReaderController {
 
     @RequestMapping(value = "/reader")
     public String ReaderName(Map<String, Object> map) {
-        map.put("reader", readerService.findById(1));
+        map.put("reader", readerService.findById(8));
         return "reader";
     }
 
-//    @RequestMapping(value = "add", method = RequestMethod.POST)
-//    public String addReader(@ModelAttribute("reader") Reader reader,BindingResult result) {
-//        readerService.saveReader(reader);
-//        return "reader";
-//    }
-//
-//    @RequestMapping("delete/{readerId}")
-//    public String deleteContact(@PathVariable("readerId") Integer readerId) {
-//        readerService.delete(readerId);
-//        return "reader";
-//    }
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String addReader(@RequestParam("name") String name, @RequestParam("surname") String surname,
+                            @RequestParam("address") String address, @RequestParam("phone") String phone,
+                            @RequestParam("birth") String birth) {
+        Reader reader = new Reader();
+        reader.setName(name);
+        reader.setSurname(surname);
+        reader.setAdress(address);
+        reader.setPhone(phone);
+        reader.setBirth(Date.valueOf(birth));
+        reader.setDateOfCreate(Date.valueOf(LocalDate.now()));
+        readerService.saveReader(reader);
+        return "redirect:/reader";
+    }
+
+    @RequestMapping("delete/{idReader}")
+    public String deleteContact(@PathVariable("idReader") Integer idReader) {
+        readerService.delete(idReader);
+        return "redirect:/reader";
+    }
 }
--- src/main/webapp/WEB-INF/views/reader.jsp
+++ src/main/webapp/WEB-INF/views/reader.jsp
@@ -16,9 +16,38 @@
     <h2>
         Менеджер читачів
     </h2>
-
+    <form method="post" action="add">
+        <table>
+            <tr>
+                <td>Ім'я</td>
+                <td><input type="text" name="name"></td>
+            </tr>
+            <tr>
+                <td>Прізвище</td>
+                <td><input type="text" name="surname"></td>
+            </tr>
+            <tr>
+                <td>Адреса</td>
+                <td><input type="text" name="address"></td>
+            </tr>
+            <tr>
+                <td>Телефон</td>
+                <td><input type="text" name="phone"></td>
+            </tr>
+            <tr>
+                <td>Дата народження</td>
+                <td><input type="text" name="birth"></td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <input type="submit" value="Зберегти читача" />
+                </td>
+            </tr>
+        </table>
+    </form>
+    <br>
     <table border="1">
-        <thead><tr style='font-weight: bold; font-size: 16pt'><td colspan='6' style='text-align: center'>Читачі</td></tr></thead>
+        <thead><tr style='font-weight: bold; font-size: 16pt'><td colspan='7' style='text-align: center'>Читачі</td></tr></thead>
         <tr>
             <th>Ім'я</th>
             <th>Прізвище</th>
@@ -26,6 +55,7 @@
             <th>Телефон</th>
             <th>Дата народження</th>
             <th>Дата реєстрації</th>
+            <th>&nbsp;</th>
         </tr>
         <tr>
             <td>${reader.name}</td>
@@ -34,6 +64,7 @@
             <td>${reader.phone}</td>
             <td>${reader.birth}</td>
             <td>${reader.dateOfCreate}</td>
+            <td><a href="delete/${reader.idReader}">Видалити</a></td>
         </tr>
     </table>
 
