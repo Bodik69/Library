9ec98095dcae0b1c84acb687742a647d93630b7d
Bodik69 <bsmachylo86@gmail.com>
2015-12-16 13:32:15 +0200

    method to find book by properties of other book to book dao

--- src/main/java/com/softserve/edu/dao/impl/BookDAOImpl.java
+++ src/main/java/com/softserve/edu/dao/impl/BookDAOImpl.java
@@ -2,6 +2,9 @@ package com.softserve.edu.dao.impl;
 
 import com.softserve.edu.dao.BookDAO;
 import com.softserve.edu.entity.Book;
+import org.hibernate.Query;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,7 +15,20 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class BookDAOImpl extends GenericDAOImpl<Book> implements BookDAO {
 
+    @Autowired
+    private SessionFactory sessionFactory;
+
     public BookDAOImpl() {
         super(Book.class);
+    }
+
+    @Override
+    public Book findBookByOtherBookProperties(Book book) {
+        Query query = sessionFactory.getCurrentSession().createQuery("FROM Book WHERE title = :title AND edition = :edition AND year = :year AND pages = :pages");
+        query.setParameter("title", book.getTitle());
+        query.setParameter("edition", book.getEdition());
+        query.setParameter("year", book.getYear());
+        query.setParameter("pages", book.getPages());
+        return (Book)query.uniqueResult();
     }
 }
