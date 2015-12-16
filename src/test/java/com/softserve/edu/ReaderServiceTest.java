1d4a38ceb4ab3b63e194c28670623b036eef4958
persik <ihorsokolyk1993@gmail.com>
2015-12-16 20:07:47 +0200

    added testng

--- src/test/java/com/softserve/edu/ReaderServiceTest.java
+++ src/test/java/com/softserve/edu/ReaderServiceTest.java
@@ -1,10 +1,75 @@
 package com.softserve.edu;
 
 
-import org.junit.Test;
+import com.softserve.edu.dao.ReaderDAO;
+import com.softserve.edu.entity.Reader;
+import com.softserve.edu.service.impl.ReaderServiceImpl;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.when;
 
 public class ReaderServiceTest {
+    @Mock
+    ReaderDAO readerDAO;
+
+    @InjectMocks
+    ReaderServiceImpl readerService;
+
+    @Spy
+    List<Reader> readers = new ArrayList<Reader>();
+
+    @BeforeClass
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        readers = getReaderList();
+    }
+
+    @Test
+    public void testFindById() {
+        Reader reader = readers.get(0);
+        when(readerDAO.find(anyInt())).thenReturn(reader);
+        Assert.assertEquals(readerService.findById(reader.getIdReader()), reader);
+    }
+
+    @Test
+    public void testSaveReader() {
+        
+    }
+
+    public List<Reader> getReaderList(){
+        Reader r1 = new Reader();
+        r1.setIdReader(1);
+        r1.setName("Ihor");
+        r1.setSurname("Sokolyk");
+        r1.setPhone("09654245851");
+        r1.setAdress("Lviv");
+        r1.setDateOfCreate((Date.valueOf(LocalDate.now())));
+        r1.setBirth((Date.valueOf(LocalDate.now())));
+
+        Reader r2 = new Reader();
+        r2.setIdReader(2);
+        r2.setName("Bohdan");
+        r2.setSurname("Smachylo");
+        r2.setPhone("09654245121");
+        r2.setAdress("Lviv");
+        r2.setDateOfCreate((Date.valueOf(LocalDate.now())));
+        r2.setBirth((Date.valueOf(LocalDate.now())));
+
+
+        readers.add(r1);
+        readers.add(r2);
+        return readers;
+    }
     }
